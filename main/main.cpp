a34575057418fcefe8b800999e1576607040132d
Filip Budiša <filip@circuitmess.com>
2025-02-13 09:46:09 +0100

    Pins and hardware setup NEV-6

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,13 @@
 #include <Core/EntryPoint.h>
 #include <Periphery/WiFi.h>
-#include "src/Services/WiFiStation.h"
-#include "src/Services/TCPClient.h"
+#include <Periphery/I2C.h>
+#include <Devices/AW9523.h>
+#include <Drivers/Output/OutputCurrAW.h>
+#include <nvs_flash.h>
+#include "HardwareConfig.h"
+#include "Pins.hpp"
+#include "Services/WiFiStation.h"
+#include "Services/TCPClient.h"
 
 class TestApp : public Application {
 	GENERATED_BODY(TestApp, Application)
@@ -9,6 +15,25 @@ class TestApp : public Application {
 protected:
 	virtual void begin() noexcept override {
 		Super::begin();
+
+		auto ret = nvs_flash_init();
+		if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND){
+			ESP_ERROR_CHECK(nvs_flash_erase());
+			ret = nvs_flash_init();
+		}
+		ESP_ERROR_CHECK(ret);
+
+		HardwareConfiguration* config = registerSingleton<HardwareConfiguration>();
+
+		I2C* i2c = registerPeriphery<I2C>(I2CPort::Zero, static_cast<gpio_num_t>(I2C_SDA), static_cast<gpio_num_t>(I2C_SCL));
+
+		AW9523* aw9523 = registerDevice<AW9523>(i2c, config->getAW9523Address());
+		aw9523->setCurrentLimit(AW9523::IMAX);
+
+		OutputCurrAW* outputCurrAW = registerService<OutputCurrAW>(config->getAW9523Outputs(), aw9523);
+		for(const auto out: config->getAW9523Outputs()){
+			outputCurrAW->write(out.port, false);
+		}
 
 		registerPeriphery<WiFi>();
 		registerService<WiFiStation>();
---
+++ main/src/HardwareConfig.h
@@ -0,0 +1,28 @@
+#ifndef NEVERA_FIRMWARE_HARDWARECONFIG_H
+#define NEVERA_FIRMWARE_HARDWARECONFIG_H
+
+#include <Misc/Singleton.h>
+#include "Drivers/Interface/OutputDriver.h"
+#include "Pins.hpp"
+
+class HardwareConfiguration : public Singleton {
+		GENERATED_BODY(HardwareConfiguration, Singleton)
+
+public:
+	uint8_t getAW9523Address() const noexcept { return AW9523Address; }
+	const std::vector<OutputPinDef>& getAW9523Outputs() const noexcept { return AW9523Outputs; }
+
+private:
+	const uint8_t AW9523Address = 0x5b;
+
+	//ports are not inverted since AW9523 led driver is a current source
+	const std::vector<OutputPinDef> AW9523Outputs = {
+			{ EXP_HEAD_L, true },
+			{ EXP_HEAD_R, true },
+			{ EXP_TAIL_L, true },
+			{ EXP_TAIL_R, true }
+	};
+
+};
+
+#endif //NEVERA_FIRMWARE_HARDWARECONFIG_H
