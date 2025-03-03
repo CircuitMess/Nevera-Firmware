76b652b26ce1e8d381ce2c6acdc8c8f58ceae8e7
Emil Gajšak <emil.gajsak@gmail.com>
2025-03-03 19:25:58 +0100

    LED Enums converted to regular enums

--- main/src/Enums.h
+++ main/src/Enums.h
@@ -1,10 +1,10 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
-DECLARE_ENUM(LEDs, HeadlightsLeft, HeadlightsRight, TaillightsLeft, TaillightsRight);
+enum class LEDs{ HeadlightsLeft, HeadlightsRight, TaillightsLeft, TaillightsRight};
 
 DECLARE_ENUM(Button, Pair);
 
-DECLARE_ENUM(RGB_LEDs, Left1, Left2, Left3, Right1, Right2, Right3);
+enum class RGB_LEDs{ Left1, Left2, Left3, Right1, Right2, Right3};
 
 #endif //ENUMS_H
