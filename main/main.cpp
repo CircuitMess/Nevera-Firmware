126a54da29ac7dd7eb87e3e18046d9237f3fc369
Fr0zen3k <jan.kresic@circuitmess.com>
2025-10-10 11:21:13 +0200

    Profiling WIP

--- main/main.cpp
+++ main/main.cpp
@@ -27,6 +27,130 @@
 #include <Util/StateMachine/StateMachine.h>
 #include "Services/Battery.h"
 
+#include <iostream>
+#include "src/lib/sprofiler/sprofiler.h"
+
+#include <Services/Modules/ModuleService.h>
+
+#define NUM_OF_SPIN_TASKS   6
+#define SPIN_ITER           500000  //Actual CPU cycles used will depend on compiler optimization
+#define SPIN_TASK_PRIO      2
+#define STATS_TASK_PRIO     3
+#define STATS_TICKS         pdMS_TO_TICKS(1000)
+#define ARRAY_SIZE_OFFSET   5   //Increase this if print_real_time_stats returns ESP_ERR_INVALID_SIZE
+
+static esp_err_t print_real_time_stats(TickType_t xTicksToWait)
+{
+    TaskStatus_t *start_array = NULL, *end_array = NULL;
+    UBaseType_t start_array_size, end_array_size;
+    uint32_t start_run_time, end_run_time;
+    esp_err_t ret;
+
+    //Allocate array to store current task states
+    start_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
+    start_array = static_cast<TaskStatus_t *>(malloc(sizeof(TaskStatus_t) * start_array_size));
+    if (start_array == NULL) {
+    	std::cout << "start_array is nullptr" << std::endl;
+        ret = ESP_ERR_NO_MEM;
+    	free(start_array);
+    	free(end_array);
+    	return ret;
+    }
+    //Get current task states
+    start_array_size = uxTaskGetSystemState(start_array, start_array_size, &start_run_time);
+    if (start_array_size == 0) {
+    	std::cout << "start_array_size is 0" << std::endl;
+        ret = ESP_ERR_INVALID_SIZE;
+    	free(start_array);
+    	free(end_array);
+    	return ret;
+    }
+
+    vTaskDelay(xTicksToWait);
+
+    //Allocate array to store tasks states post delay
+    end_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
+    end_array = static_cast<TaskStatus_t *>(malloc(sizeof(TaskStatus_t) * end_array_size));
+    if (end_array == NULL) {
+    	std::cout << "end_array is nullptr" << std::endl;
+        ret = ESP_ERR_NO_MEM;
+    	free(start_array);
+    	free(end_array);
+    	return ret;
+    }
+    //Get post delay task states
+    end_array_size = uxTaskGetSystemState(end_array, end_array_size, &end_run_time);
+    if (end_array_size == 0) {
+    	std::cout << "end_array_size is 0" << std::endl;
+        ret = ESP_ERR_INVALID_SIZE;
+    	free(start_array);
+    	free(end_array);
+    	return ret;
+    }
+
+    //Calculate total_elapsed_time in units of run time stats clock period.
+    uint32_t total_elapsed_time = (end_run_time - start_run_time);
+    if (total_elapsed_time == 0) {
+    	std::cout << "total_elapsed_time is 0" << std::endl;
+        ret = ESP_ERR_INVALID_STATE;
+    	free(start_array);
+    	free(end_array);
+    	return ret;
+    }
+
+    std::cout << "| Task | Run Time | Percentage\n" << std::endl;
+    //Match each task in start_array to those in the end_array
+    for (int i = 0; i < start_array_size; i++) {
+        int k = -1;
+        for (int j = 0; j < end_array_size; j++) {
+            if (start_array[i].xHandle == end_array[j].xHandle) {
+                k = j;
+                //Mark that task have been matched by overwriting their handles
+                start_array[i].xHandle = NULL;
+                end_array[j].xHandle = NULL;
+                break;
+            }
+        }
+        //Check if matching task found
+        if (k >= 0) {
+            uint32_t task_elapsed_time = end_array[k].ulRunTimeCounter - start_array[i].ulRunTimeCounter;
+            uint32_t percentage_time = (task_elapsed_time * 100UL) / (total_elapsed_time * portNUM_PROCESSORS);
+        	std::cout << "| " << start_array[i].pcTaskName << " | " << task_elapsed_time << " | " << percentage_time << "%" << std::endl;
+        }
+    }
+
+    //Print unmatched tasks
+    for (int i = 0; i < start_array_size; i++) {
+        if (start_array[i].xHandle != NULL) {
+        	std::cout << "| " << start_array[i].pcTaskName << " | Deleted" << std::endl;
+        }
+    }
+    for (int i = 0; i < end_array_size; i++) {
+        if (end_array[i].xHandle != NULL) {
+        	std::cout << "| " << end_array[i].pcTaskName << " | Created" << std::endl;
+        }
+    }
+    ret = ESP_OK;
+
+	free(start_array);
+	free(end_array);
+	return ret;
+}
+
+static void stats_task(void *arg)
+{
+	//Print real time stats periodically
+	while (true) {
+		if(print_real_time_stats(STATS_TICKS) == ESP_OK){
+			printf("Real time stats obtained\n");
+		}else{
+			printf("Error getting real time stats\n");
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
 class Nevera : public Application {
 	GENERATED_BODY(Nevera, Application)
 
@@ -36,6 +160,9 @@ public:
 protected:
 	virtual void begin() noexcept override {
 		Super::begin();
+
+		//sprofiler_initialize(100);
+		//xTaskCreatePinnedToCore(stats_task, "stats", 4096, NULL, STATS_TASK_PRIO, NULL, tskNO_AFFINITY);
 
 		auto ret = nvs_flash_init();
 		if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND){
@@ -89,6 +216,15 @@ protected:
 
 		ledService->reg(ledPins);
 		ledService->reg(rgbleds);
+
+		ledService->on(RGB_LEDs::Left1, { 0.0f, 1.0f, 0.0f });
+		ledService->on(RGB_LEDs::Right1, { 0.0f, 1.0f, 0.0f });
+
+		ledService->on(RGB_LEDs::Left2, { 0.0f, 1.0f, 0.0f });
+		ledService->on(RGB_LEDs::Right2, { 0.0f, 1.0f, 0.0f });
+
+		ledService->on(RGB_LEDs::Left3, { 0.0f, 1.0f, 0.0f });
+		ledService->on(RGB_LEDs::Right3, { 0.0f, 1.0f, 0.0f });
 
 		auto i2s = registerPeriphery<I2S>(I2S_NUM_AUTO, config->getI2SConfig());
 
